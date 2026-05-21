03059d6c37bc0eb638b12cae763923960eebdffa
Adrian Hito <ahito246@gmail.com>
2026-05-21 03:52:43 -0400

    Fix Geography types with proper interface

--- src/types/react-simple-maps.d.ts
+++ src/types/react-simple-maps.d.ts
@@ -1,8 +1,15 @@
 declare module 'react-simple-maps' {
   import { ComponentType } from 'react';
 
+  export interface Geography {
+    id: string;
+    rsmKey: string;
+    properties: any;
+    [key: string]: any;
+  }
+
   export interface GeographyProps {
-    geography: any;
+    geography: Geography;
     fill?: string;
     stroke?: string;
     strokeWidth?: number;
@@ -15,8 +22,13 @@ declare module 'react-simple-maps' {
     [key: string]: any;
   }
 
+  export interface GeographiesProps {
+    geography: string;
+    children: (props: { geographies: Geography[] }) => React.ReactNode;
+  }
+
   export const ComposableMap: ComponentType<any>;
-  export const Geographies: ComponentType<any>;
+  export const Geographies: ComponentType<GeographiesProps>;
   export const Geography: ComponentType<GeographyProps>;
   export const ZoomableGroup: ComponentType<any>;
 }
